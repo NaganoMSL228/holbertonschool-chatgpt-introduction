dbd086800eb71b3d1213035d1ad77a2d16fff715
NaganoMSL228 <9794@holbertonstudents.com>
2025-01-13 10:15:49 +0100

    agin new script for the task one

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -2,20 +2,11 @@
 import sys
 
 def factorial(n):
-    if n == 0:  # Cas où n est égal à 0
-        return 1
     result = 1
     while n > 1:
         result *= n
-        n -= 1
+        n -= 1  # Decrement n to avoid infinite loop
     return result
 
-if len(sys.argv) != 2:
-    print("Veuillez fournir un argument.")
-    sys.exit(1)
-
-try:
-    f = factorial(int(sys.argv[1]))
-    print(f)
-except ValueError:
-    print("Veuillez entrer un nombre entier valide.")
+f = factorial(int(sys.argv[1]))
+print(f)
