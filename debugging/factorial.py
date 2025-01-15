ebbe0e1dbd7608be8e31de972e6d21c826ffdadc
NaganoMSL228 <9794@holbertonstudents.com>
2025-01-15 14:34:55 +0100

    change the script one

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -2,23 +2,11 @@
 import sys
 
 def factorial(n):
-    if n < 0:
-        return "Factorial is not defined for negative numbers"
     result = 1
     while n > 1:
         result *= n
-        n = n - 1
+        n -= 1  # Corrected to decrement n
     return result
 
-if len(sys.argv) != 2:
-    print("Usage: python3 script_name.py <number>")
-    sys.exit(1)
-
-try:
-    number = int(sys.argv[1])
-except ValueError:
-    print("Error: Input must be an integer")
-    sys.exit(1)
-
-f = factorial(number)
+f = factorial(int(sys.argv[1]))
 print(f)
