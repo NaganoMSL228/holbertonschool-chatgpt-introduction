e7c37f9b98620e638ecb0642f84b1a18ac205521
NaganoMSL228 <9794@holbertonstudents.com>
2025-01-15 01:06:14 +0100

    two first task

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,18 +5,8 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1  # Décrémente n à chaque itération
+        n -= 1  # Decrement n here
     return result
 
-if len(sys.argv) > 1:
-    try:
-        n = int(sys.argv[1])
-        if n < 0:
-            print("Le factoriel n'est pas défini pour les nombres négatifs.")
-        else:
-            f = factorial(n)
-            print(f)
-    except ValueError:
-        print("Veuillez entrer un nombre entier valide.")
-else:
-    print("Veuillez fournir un argument numérique.")
+f = factorial(int(sys.argv[1]))
+print(f)
